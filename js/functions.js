6157421e74b4be1fd8e9a38f57d68d56d559ad73
Alice <alice.ashford@gmail.com>
2021-06-18 16:24:16 -0500

    mobile menu hamburguer

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,28 @@
+let slider = document.getElementById("main-slider");
+let header = document.getElementById("header");
+let hamburguer = document.getElementById("hamburguer");
+let tache = document.getElementById("tache");
+
+function closeMenu() {
+    header.classList.add("hidden");
+    header.classList.remove("show");
+    hamburguer.classList.add("show");
+
+}
+
+function showMenu() {
+    header.classList.add("show");
+    header.classList.remove("hidden");
+    hamburguer.classList.add("hidden");
+    hamburguer.classList.remove("show");
+    tache.classList.remove("hidden");
+}
+
 function pintar() {
     var element = document.getElementById("menu-drop");
     element.classList.remove("oculto");
 }
 
-
-let slider = document.getElementById("main-slider");
-let header = document.getElementById("header");
 
 
 
